19721879d35dd59574ed4936f633a82e450d3a5c
Imari McKinney Jr <imarijr7@gmail.com>
2018-05-22 14:50:03 -0400

    add styling and components to main

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
+import Sidebar from'./Sidebar'
+import NoteList from './NoteList'
+import NoteForm from './NoteForm'
+
+
 const Main = () => {
     return (
-        <div className="Main">
-            Main!
+        <div className="Main"
+        style ={style}>
+            <Sidebar />
+            <NoteList />
+            <NoteForm />
         </div>
 
     
     )
+}
+
+const style = {
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'stretch',
 }
 
 export default Main

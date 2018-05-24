5506e668c7769d6a6e426a3bd9c1bae77568171b
Imari McKinney Jr <imarijr7@gmail.com>
2018-05-24 10:53:32 -0400

    add finished styling and make delete button work

--- src/Main.js
+++ src/Main.js
@@ -1,27 +1,91 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import Sidebar from'./Sidebar'
+import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
+class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentNote: this.blankNote(),
+      notes: [],
+    }
+  }
 
-const Main = () => {
-    return (
-        <div className="Main"
-        style ={style}>
-            <Sidebar />
-            <NoteList />
-            <NoteForm />
-        </div>
+  blankNote = () => {
+    return {
+      id: null,
+      title: '',
+      body: '',
+    }
+  }
+
+  setCurrentNote = (note) => {
+    this.setState({ currentNote: note })
+  }
+
+  resetCurrentNote = () => {
+    this.setCurrentNote(this.blankNote())
+  }
+
+  deleteNote = () => {
+    const notes = [...this.state.notes]
+
+    const i = notes.findIndex(note => note.id === this.state.currentNote.id)
+    if(i >-1){
+        notes.splice(i,1)
+        this.setState({ notes })
+    }
+    
+    this.resetCurrentNote
+    
 
     
+}
+
+  saveNote = (note) => {
+    const notes = [...this.state.notes]
+
+    if (note.id) {
+      
+        
+      const i = notes.findIndex(currentNote => currentNote.id === note.id)
+      notes[i] = note
+    } else {
+      // new note
+      note.id = Date.now()
+      notes.push(note)
+    }
+    localStorage.setItem('notes', JSON.stringify(notes))
+    this.setState({ notes, currentNote: note })
+  }
+
+  render() {
+    return (
+      <div
+        className="Main"
+        style={style}
+      >
+        <Sidebar resetCurrentNote={this.resetCurrentNote} />
+        <NoteList
+          notes={this.state.notes}
+          setCurrentNote={this.setCurrentNote}
+        />
+        <NoteForm
+          currentNote={this.state.currentNote}
+          saveNote={this.saveNote}
+          deleteNote = {this.deleteNote}
+        />
+      </div>
     )
+  }
 }
 
 const style = {
-    display: 'flex',
-    height: '100vh',
-    alignItems: 'stretch',
+  display: 'flex',
+  height: '100vh',
+  alignItems: 'stretch',
 }
 
 export default Main
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,124 +1,103 @@
 import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
 
 import quill from './quill.svg'
 import newIcon from './new.png'
 import newHover from './new-hover.png'
 
-const Sidebar = () => {
-    return(
-        <nav 
-        className="Sidebar"
-        style= {styles.sidebar}>
-          <div 
-          className="logo"
-          style = {styles.logo}>
-            <img 
-            src={quill} 
-            alt="Noteherder"
-            style ={styles.logoImg}/>
-
-          </div>
-          <a           
-          className="new-note" 
-          href="/notes"
-          style = {styles.newNote}
-          onMouseEnter = {onHover}
-          onMouseLeave = {offHover}
-           >
-            <img 
-            src={newHover} 
-            alt="New note"
-            style = {styles.image}
-            />
-            <img 
-            className="outline" 
-            src={newIcon} 
-            alt="New note"
-            style = {styles.image}
-            
-            />
-          </a>
-          <div className="SignOut"
-          style = {styles.signOut}>
-            <button 
-            style={styles.button}>
-              <i className="fa fa-sign-out"
-              style={styles.iFa}
-              ></i>
-            </button>
-          </div>
-        </nav>
-    )
+const Sidebar = ({ resetCurrentNote }) => {
+  return (
+    <nav className={css(styles.sidebar)}>
+      <div
+        className={css(styles.logo)}
+      >
+        <img
+          src={quill}
+          alt="Noteherder"
+          className={css(styles.logoImg)}
+        />
+      </div>
+      <a
+        href="/notes"
+        className={css(styles.newNote)}
+        onClick={(ev) => {
+          ev.preventDefault()
+          resetCurrentNote()
+        }}
+      >
+        <img
+          src={newHover}
+          alt="New note"
+          className={css(styles.newNoteImg)}
+        />
+        <img
+          src={newIcon}
+          alt="New note"
+          className={css(styles.newNoteImg, styles.newNoteImgHover)}
+        />
+      </a>
+      <div
+        className={css(styles.signOut)}
+      >
+        <button className={css(styles.button)}>
+          <i
+            className={`fas fa-sign-out-alt ${css(styles.buttonIcon)}`}
+            title="sign out"
+          ></i>
+        </button>
+      </div>
+    </nav>
+  )
 }
 
-function onHover(){
-    
-   const a = document.querySelector('.new-note').children[1].style.opacity = 0
-
-
-}
-function offHover(){
-    const b = document.querySelector('.new-note').children[1].style.opacity = 1
-}
-
-
-
-const styles = {
-    sidebar:{
-        width:'6rem',
-        backgroundColor: '#f3f3f3',
-        padding: '0.5rem 0',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
+const styles = StyleSheet.create({
+  sidebar: {
+    width: '6rem',
+    backgroundColor: '#f3f3f3',
+    padding: '0.5rem 0',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  logo: {
+    fontFamily: '"Fauna One"',
+    color: '#666',
+    fontSize: '3rem',
+  },
+  logoImg: {
+    width: '3rem',
+    paddingLeft: '0.4rem',
+  },
+  newNote: {
+    marginTop: '2rem',
+    position: 'relative',
+    width: '4rem',
+  },
+  newNoteImg: {
+    position: 'absolute',
+    left: '0',
+    width: '100%',
+    transition: 'opacity 0.25s ease-in-out',
+  },
+  newNoteImgHover: {
+    ':hover': {
+      opacity: 0,
     },
-
-    logo:{
-        fontFamily: '"Fuana One"',
-        color: '#666',
-        fontSize: '3rem',
-    },
-
-    logoImg: {
-        width: '3rem',
-        paddingLeft: '0.4rem'
-    },
-
-    newNote: {
-        marginTop: '2rem',
-        position: 'relative',
-        width: '4rem'
-    
-    },
-    button:{
-        backgroundColor: 'transparent',
-        border: '0',
-        color: '#008BF8',
-        cursor: 'pointer'
-    },
-    image:{
-        position:'absolute',
-        left: '0',
-        width: '100%',
-        transition: 'opacity 0.25s ease-in-out',
-
-    },
-    
-    signOut:{
-        position: 'absolute',
-        bottom: '1rem'
-    },
-    iFa:{
-        fontSize: '2rem'
-    },
-    signOutButton: {
-        outline:'none'
-
-    },
-    
-
-
-
-}
+  },
+  signOut: {
+    position: 'absolute',
+    bottom: '1rem',
+  },
+  button: {
+    backgroundColor: 'transparent',
+    border: '0',
+    color: '#008bf8',
+    cursor: 'pointer',
+    outline: 'none',
+  },
+  buttonIcon: {
+    fontSize: '2rem',
+  },
+})
 
 export default Sidebar

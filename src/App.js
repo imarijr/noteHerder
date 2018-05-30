3439aa531aaefa510447c8100e56a915cc456167
Imari McKinney Jr <imarijr7@gmail.com>
2018-05-30 14:15:34 -0400

    add redirect if not signed in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom'
 
 import './App.css' 
 import {auth} from './base'
@@ -50,13 +51,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.signedIn()
-        ? <Main signOut={this.signOut} uid={this.state.uid}/>
-        : <SignIn />
-        }
+        <Switch>
+          <Route
+            path="/sign-in"
+            render={() => (
+              this.signedIn()
+                ? <Redirect to="/notes" />
+                : <SignIn />
+            )}
+          />
+          <Route
+            path="/notes"
+            render={() => (
+              this.signedIn()
+                ? <Main signOut={this.signOut} uid={this.state.uid} />
+                : <Redirect to="/sign-in" />
+            )}
+          />
+        </Switch>
+        {/*
+          this.signedIn()
+            ? <Main signOut={this.signOut} uid={this.state.uid} />
+            : <SignIn />
+        */}
+
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
